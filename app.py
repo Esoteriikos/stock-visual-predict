115a9de8d017d73c32099ed7074acc19cc9f0739
Esoteriikos <32803442+Esoteriikos@users.noreply.github.com>
2021-05-11 15:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import streamlit as st
 from datetime import date
 import yfinance as yf
 from plotly import graph_objs as go
+import plotly
+import pandas as pd
+import numpy as np
+import json
+from flask import Flask, render_template, request, send_from_directory, Response
+
+app = Flask(__name__)
+
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Historical Stock price visualization and forecasting")
-st.write("author: Siddhesh pawar")
 
 stocks = ["TCS.NS","LT.NS", "LTI.NS", "LTI.BO", "HDFCBANK.NS","HDFC.NS", "INFY.NS","TECHM.NS","WIPRO.NS", "RELIANCE.NS", "HINDUNILVR.NS", "ITC.NS", "MARUTI.NS", "MCDOWELL-N.NS", "AMBUJACEM.NS", "SIEMENS.NS", "TATAMOTORS.NS", "TATASTEEL.NS", "TATAPOWER.NS", "HCLTECH.NS", "SBIN.NS", "ONGC.NS"]
 
-selected_stock = st.selectbox("Select Company for prediction", stocks)
-
-
-@st.cache
 #load stock data. 
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
@@ -23,122 +24,27 @@ def load_data(ticker):
     #puts date in first column
     return data
 
-data_load_state = st.text("Loading data...")
-data = load_data(selected_stock)
 
-data_load_state.text("Loading data done!") #See streamlit tut
+@app.route('/')
+def home():
+    return render_template('home.html', stocks=stocks)
 
-
-#Plot data
-
-
-
-def plot_raw_data():
+@app.route('/process', methods =["GET", "POST"])
+def process():
+    #stock_name = "haha"
+    #if request.method == "POST":
+    stock_name = request.args.get('stock_name')
+    data = load_data(str(stock_name))
+    print(data)
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y = data['Open'], name = 'stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y = data['Close'], name = 'stock_close'))
     fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+    #fig.show()
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-plot_raw_data()
+    return render_template('process.html',
+                       graphJSON=graphJSON)
 
-st.subheader("Forecast time series data using Facebook Prophet")
-n_years = st.slider("Years of Prediction:", 1, 4)
-period = n_years*365
-
-#Forecasting
-
-data_load_state = st.text("Forecasting data...Please wait")
-
-df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns = {"Date":"ds", "Close":"y"})
-
-
-
-# import streamlit as st
-# from datetime import date
-# import yfinance as yf
-# #from fbprophet import Prophet
-# #from fbprophet.plot import plot_plotly
-# from plotly import graph_objs as go
-
-# START = "2015-01-01"
-# TODAY = date.today().strftime("%Y-%m-%d")
-
-# st.title("Historical Stock price visualization and forecasting")
-
-# stocks = ["TCS.NS","LT.NS", "LTI.NS", "LTI.BO", "HDFCBANK.NS","HDFC.NS", "INFY.NS","TECHM.NS","WIPRO.NS", "RELIANCE.NS", "HINDUNILVR.NS", "ITC.NS", "MARUTI.NS", "MCDOWELL-N.NS", "AMBUJACEM.NS", "SIEMENS.NS", "TATAMOTORS.NS", "TATASTEEL.NS", "TATAPOWER.NS", "HCLTECH.NS", "SBIN.NS", "ONGC.NS"]
-
-# selected_stock = st.selectbox("Select Company for prediction", stocks)
-
-
-# @st.cache
-# #load stock data. 
-# def load_data(ticker):
-#     data = yf.download(ticker, START, TODAY)
-#     #returns pandas df
-#     data.reset_index(inplace = True)
-#     #puts date in first column
-#     return data
-
-# data_load_state = st.text("Loading data...")
-# data = load_data(selected_stock)
-
-# data_load_state.text("Loading data done!") #See streamlit tut
-
-
-# #Plot data
-
-
-
-# def plot_raw_data():
-#     fig = go.Figure()
-#     fig.add_trace(go.Scatter(x=data['Date'], y = data['Open'], name = 'stock_open'))
-#     fig.add_trace(go.Scatter(x=data['Date'], y = data['Close'], name = 'stock_close'))
-#     fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible=True)
-#     st.plotly_chart(fig)
-
-# plot_raw_data()
-
-# st.subheader("Forecast time series data using Facebook Prophet")
-# n_years = st.slider("Years of Prediction:", 1, 4)
-# period = n_years*365
-
-# #Forecasting
-
-# data_load_state = st.text("Forecasting data...Please wait")
-
-# df_train = data[['Date', 'Close']]
-# df_train = df_train.rename(columns = {"Date":"ds", "Close":"y"})
-
-
-
-
-#m = Prophet()
-#m.fit(df_train)
-
-#future = m.make_future_dataframe(periods = period)
-#forecast = m.predict(future)
-
-#st.write('Forecast data')
-#fig1 = plot_plotly(m, forecast)
-#st.plotly_chart(fig1)
-
-#st.write('forecast components')
-#fig2 = m.plot_components(forecast)
-#st.write(fig2)
-
-#data_load_state.text("Forecasting data done!")
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True, threaded=True, use_reloader=False)
